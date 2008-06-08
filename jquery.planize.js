35310ada97065a4e90f82f260541edaf3f65a730
n1k0 <nperriault@gmail.com>
2008-06-08 13:41:44 +0000

    internal log() function that uses either firebug or opera console if available

--- jquery.planize.js
+++ jquery.planize.js
@@ -4,7 +4,7 @@
  *  - generates an HTML table of content
  * @author  Nicolas Perriault <nperriault -> gmail.com>
  * @license MIT (http://www.opensource.org/licenses/mit-license.php)
- * @return  $(this)
+ * @return  jQuery(this)
  */
 jQuery.fn.planize = function(config) {
   
@@ -22,14 +22,15 @@ jQuery.fn.planize = function(config) {
     toc_elem       : null,       // the dom element where the toc will be append
     toc_list_class : 'plan_toc', // the class name of the toc list element
     max_level      : 0,          // max depth level to generate a toc and header numbering (0 = all depths)
+    debug          : false,      // prints debug messages into firebug or opera console
   };
-  config = $.extend(defaultConfig, config);
+  config = jQuery.extend(defaultConfig, config);
   
   /**
    * Prepends all headers text with the current tree number reference
    * @return void
    */
-  this.process = function() {
+  var process = function() {
     var level       = 0;
     var levels      = [0,0,0,0,0,0,0];
     var hLevelText  = '';
@@ -37,53 +38,64 @@ jQuery.fn.planize = function(config) {
     var prevLevel   = 0;
     self.children('*:header').each(function(index) {
       level = parseInt(this.tagName.substring(1));
-      levels[level]++;
-      for (var l = 1; l <= level; l++) {
-        hLevelText += levels[l] + config.sep;
-      }
-      levels[level + 1] = 0;
-      hLevelText = hLevelText.substring(0, hLevelText.length - 1);
-      prependText = hLevelText;
-      if (config.generate_toc || config.add_anchors) {
-        if (config.generate_toc) {
-          var elem = "\n"+'<li><a href="#h' + hLevelText + '">' + hLevelText + (config.separator ? config.separator : '') + ' ' + $(this).text() + '</a>';
-          if (level < prevLevel) {
-            console.log(hLevelText + ', unnesting because:' + level + '<' + prevLevel);
-            var unnest = '';
-            while (level < prevLevel) {
-              unnest += '</ul>';
-              prevLevel--;
-            }
-            toc += unnest + elem + '</li>';
-          } else if (level > prevLevel) {
-            console.log(hLevelText + ', nesting because:' + level + '>' + prevLevel);
-            toc += '<ul>' + elem;
-          } else {
-            console.log(hLevelText + ', same level (' + level + ')');
-            toc += elem;
-          }
+      if (config.max_level <= 0 || level <= config.max_level) {
+        levels[level]++;
+        for (var l = 1; l <= level; l++) {
+          hLevelText += levels[l] + config.sep;
         }
-        prependText = '<span id="h' + hLevelText + '"></span>' + hLevelText;
+        levels[level + 1] = 0;
+        hLevelText = hLevelText.substring(0, hLevelText.length - 1);
+        prependText = hLevelText;
+        if (config.generate_toc || config.add_anchors) {
+          if (config.generate_toc) {
+            var elem = "\n"+'<li><a href="#h' + hLevelText + '">' + hLevelText + (config.separator ? config.separator : '') + ' ' + jQuery(this).text() + '</a>';
+            if (level < prevLevel) {
+              log(hLevelText + ', unnesting because:' + level + '<' + prevLevel);
+              var unnest = '';
+              while (level < prevLevel) {
+                unnest += '</ul>';
+                prevLevel--;
+              }
+              toc += unnest + elem + '</li>';
+            } else if (level > prevLevel) {
+              log(hLevelText + ', nesting because:' + level + '>' + prevLevel);
+              toc += '<ul>' + elem;
+            } else {
+              log(hLevelText + ', same level (' + level + ')');
+              toc += elem;
+            }
+          }
+          prependText = '<span id="h' + hLevelText + '"></span>' + hLevelText;
+        }
+        if (config.separator) {
+          prependText += config.separator;
+        }
+        jQuery(this).prepend(prependText + ' ');
+        prependText = hLevelText = '';
+        prevLevel = level;
       }
-      if (config.separator) {
-        prependText += config.separator;
-      }
-      jQuery(this).prepend(prependText + ' ');
-      prependText = hLevelText = '';
-      prevLevel = level;
     });
     processed = true;
   };
   
-  /**
-   * Returns the HTML Table Of Content of the parsed document tree
-   * @return string
-   */
-  this.getHtmlToc = function() {
-    return toc;
+  var log = function() {
+    if (!config.debug) {
+      return;
+    }
+    try {
+      console.log.apply(console, arguments);
+    } catch(e) {
+      try {
+        opera.postError.apply(opera, arguments);
+      } catch(e){}
+    }
   }
   
-  this.process();
-  $('body').prepend(this.getHtmlToc());
+  process();
+  
+  if (config.generate_toc) {
+    jQuery(config.toc_elem ? config.toc_elem : 'body').prepend(toc);
+  }
+  
   return jQuery(this);
 }
